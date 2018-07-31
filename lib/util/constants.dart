2431e26028c539abb78468d040d391fd6257af33
josedraco <36608659+josedraco@users.noreply.github.com>
2018-07-31 13:34:46 +0200

    Update constants.dart
    key

--- lib/util/constants.dart
+++ lib/util/constants.dart
@@ -1,2 +1,2 @@
 // TODO: To run the app you need an api-key. Get one here: https://developers.themoviedb.org/3/getting-started/introduction
-const String API_KEY = "b1b1a21f789976fae1b05d3552e4273e";
+const String API_KEY = <KEY_HERE>;
